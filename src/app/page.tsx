10298a6341ec67c17149fca2411e3fcc0743da41
saurabh007007 <loverboybhol143@gmail.com>
2025-02-02 14:32:53 +0530

    initial prisma commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
  return (
-  <div>HI there </div>
+  <div>HI there <Button >Discuss App</Button></div>
  )
 }
